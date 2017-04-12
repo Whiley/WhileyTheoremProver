8490746273479f9e6b1bd53ee5b5dbce2c252e96
DavePearce <david.pearce@ecs.vuw.ac.nz>
2017-04-12 13:24:14 +1200

    Added Apache 2.0 license
    This adds the Apache 2.0 license to the WhileyTheoremProver which was
    previously unlicensed.

--- src/test/java/wyal/testing/tests/ValidTest.java
+++ src/test/java/wyal/testing/tests/ValidTest.java
@@ -1,3 +1,16 @@
+// Copyright 2017 David J. Pearce
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package wyal.testing.tests;
 
 import static org.junit.Assert.fail;
@@ -47,8 +60,6 @@ public class ValidTest {
 		IGNORED.put("test_array_50", "??");
 		IGNORED.put("test_array_60", "??");
 		IGNORED.put("test_array_61", "???");
-		IGNORED.put("test_array_66", "??");
-		IGNORED.put("test_array_67", "??");
 		//
 		IGNORED.put("test_arith_12", "#2");
 		//
