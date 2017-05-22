dee761a957f32f562046b5a28cf321a8f6cb274e
DavePearce <david.pearce@ecs.vuw.ac.nz>
2017-05-22 16:21:01 +0100

    Ignore test cases for #103

--- src/test/java/wyal/testing/tests/ValidTest.java
+++ src/test/java/wyal/testing/tests/ValidTest.java
@@ -103,6 +103,9 @@ public class ValidTest {
 		//
 		IGNORED.put("test_array_72", "#101");
 		IGNORED.put("test_ref_09", "#101");
+		//
+		IGNORED.put("test_type_95", "#103");
+		IGNORED.put("test_type_96", "#103");
 	}
 
 	// ======================================================================
