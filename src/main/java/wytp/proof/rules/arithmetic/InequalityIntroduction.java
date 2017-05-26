21babbafb016d8398cfab29ab51512fcd1956137
DavePearce <david.pearce@ecs.vuw.ac.nz>
2017-05-26 17:28:01 +0100

    Refactoring package structure

--- src/main/java/wytp/proof/rules/arithmetic/InequalityIntroduction.java
+++ src/main/java/wytp/proof/rules/arithmetic/InequalityIntroduction.java
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package wytp.proof.rules;
+package wytp.proof.rules.arithmetic;
 
 import java.util.Arrays;
 import wyal.lang.NameResolver.ResolutionError;
