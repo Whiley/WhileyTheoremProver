fa08769fe4aa33815935125d7d67305a862b8da7
DavePearce <david.pearce@ecs.vuw.ac.nz>
2017-06-01 11:20:41 +0100

    Bug fix for detecting recursive assignments #111
    This implements a simple bug fix for detecting recursive assignments.
    However, this doesn't yet prevent recursive macro expansions which are
    still causing a problem.

--- src/main/java/wytp/proof/rules/CongruenceClosure.java
+++ src/main/java/wytp/proof/rules/CongruenceClosure.java
@@ -408,13 +408,13 @@ public class CongruenceClosure extends AbstractClosureRule implements Proof.Line
 	 * @param child
 	 * @return
 	 */
-	private static boolean isParentOf(Expr parent, Expr child) {
+	private static boolean isParentOf(SyntacticItem parent, Expr child) {
 		if (parent.equals(child)) {
 			return true;
 		} else {
 			for (int i = 0; i != parent.size(); ++i) {
 				SyntacticItem pChild = parent.getOperand(i);
-				if (pChild instanceof Expr && isParentOf((Expr) pChild, child)) {
+				if (pChild != null && isParentOf(pChild, child)) {
 					return true;
 				}
 			}
